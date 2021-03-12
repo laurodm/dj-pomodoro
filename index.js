0bacdaa278b5393add10036bf9b0f00b67b17015
laurodm <laurodamasceno@gmail.com>
2021-03-11 22:13:23 -0300

    adiciona controles

--- index.js
+++ index.js
@@ -1,23 +1,52 @@
-let m = 25
-let s = 00
+const currentPomodoro = { m: 0, s: 5 }
+let pointer = null
+const pomodoro = [
+    { m: 0, s: 5 },
+    { m: 0, s: 3 }
+]
 
-setInterval(function () {
+let timeLoop = null
 
-    console.log(formatTimeString(m, s))
+function setClockValue () {
+    const { m, s } = currentPomodoro
+    const clock = document.querySelector('.clock')
+    clock.innerHTML = formatTimeString(m, s)
+}
+
+function startClock () {
+    timeLoop = setInterval(function () {
+        timeCount()
+        setClockValue()
+    }, 1000)
+}
+
+function stopClock () {
+    clearInterval(timeLoop)
+    currentPomodoro.m = 0
+    currentPomodoro.s = 5
+    setClockValue()
+}
+
+function pauseClock () {
+    clearInterval(timeLoop)
+}
+
+function timeCount () {
+    let { m, s } = currentPomodoro
 
     if (m == 0 && s == 0) {
-        clearInterval(this)
-    } else if (m > 0 && s == 0) {
+        clearInterval(timeLoop)
+    } else if (m > 0 && s === 0) {
         m -= 1
-    }
-
-    if (s == 0) {
+    } else if (m !== 0 && s === 0) {
         s = 59
     } else if (s > 0) {
         s -= 1
     }
 
-}, 1000)
+    currentPomodoro.m = m
+    currentPomodoro.s = s
+}
 
 function formatTimeString (minutes, secounds) {
     return `${formatTimeNumber(minutes)}:${formatTimeNumber(secounds)}`
@@ -25,4 +54,27 @@ function formatTimeString (minutes, secounds) {
 
 function formatTimeNumber (number) {
     return number < 10 ? '0' + number : number
+}
+
+function setControllsEvents () {
+    const startBtn = document.querySelector('#startBtn')
+    const pauseBtn = document.querySelector('#pauseBtn')
+
+    startBtn.addEventListener('click', () => {
+        startClock()
+    })
+
+    pauseBtn.addEventListener('click', () => {
+        pauseClock()
+    })
+
+    stopBtn.addEventListener('click', () => {
+        stopClock()
+    })
+}
+
+
+window.onload = function () {
+    setClockValue()
+    setControllsEvents()
 }
